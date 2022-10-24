5071b2c93d345860fe289d9ff5c164fafcc1d440
ShibLine <tiane.quelquun34@gmail.com>
2022-10-24 16:05:27 +0200

    Exo python

---
+++ Theme 1.py
@@ -0,0 +1,77 @@
+Theme 1
+print "bonjour bonsoir"
+print("bonjour c'est moi")
+print "Hello " + "tiane"
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
+todays_date = "jeudi"
+
+trippy_multiplication = 12 * 80
+product = trippy_multiplication
+print(product)
+
+remainder = 1398 % 11
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+annual_rainfall += september_rainfall
+october_rainfall = 7.20
+annual_rainfall += october_rainfall
+november_rainfall = 5.06
+annual_rainfall += november_rainfall
+december_rainfall = 4.06
+annual_rainfall += december_rainfall
+
+city_name = "St. Potatosburg"
+#la variable contient 340000
+city_pop = 340000
+
+cucumbers = 3 
+price_per_cucumber = 3.25
+
+total_cost = 3.25 * 3 
+
+print(total_cost)
+
+cucumbers = 100 
+num_people = 6 
+
+whole_cucumbers_per_person = cucumbers / num_people 
+print(whole_cucumbers_per_person)
+
+float_cucumbers_per_person = float(cucumbers) / num_people
+print(float_cucumbers_per_person)
+
+haiku = """The old pond,
+A frog jumps in:
+Plop!"""
+
+print haiku
+
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+age_is_12 = False 
+name_is_maria = True
+
+loat_1 = 0.25
+float_2 = 40.0
+
+product = float_1 * float_2
+
+big_string = "The product was " + str(product)
+skill_completed = "Python Syntax"
+exercises_completed = 13
+
+points_per_exercise = 5
+point_total = 100
+point_total += exercises_completed*points_per_exercise
+
+print("I got "+str(point_total)+" points!")
